52b799156ea594aa637dab88762af1cfa1965f40
Valentin Lorentz <progval+git@progval.net>
2017-03-23 18:08:32 +0100

    Compile Raspbian kernel instead of using Raspbian's prebuilt one.

--- customize.sh
+++ customize.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-SOURCEDIR=$(dirname $0)
+SOURCEDIR="$(dirname $0)"
+LINUXSOURCEDIR="$SOURCEDIR/linux-armhf/"
+KERNEL_ARCH=arm
 ROOTDIR="$1"
 
 # Do not start services during installation.
@@ -26,8 +28,8 @@ cp $SOURCEDIR/etc/systemd/system/regen-ssh-keys.service $ROOTDIR/etc/systemd/sys
 chroot $ROOTDIR systemctl enable regen-ssh-keys
 
 # Configure.
-cp $SOURCEDIR/boot/cmdline.txt $ROOTDIR/boot/cmdline.txt
-cp $SOURCEDIR/boot/config.txt $ROOTDIR/boot/config.txt
+cp $SOURCEDIR/boot/cmdline.txt $ROOTDIR/boot/firmware/cmdline.txt
+cp $SOURCEDIR/boot/config.txt $ROOTDIR/boot/firmware/config.txt
 cp -r $SOURCEDIR/etc/default $ROOTDIR/etc/default
 cp $SOURCEDIR/etc/fstab $ROOTDIR/etc/fstab
 cp $SOURCEDIR/etc/modules $ROOTDIR/etc/modules
@@ -43,16 +45,36 @@ else
     sed -i "s/.*PermitRootLogin.*/PermitRootLogin yes/" $ROOTDIR/etc/ssh/sshd_config
 fi
 
-# Install kernel.
+# Install Raspbian code.
 mkdir -p $ROOTDIR/lib/modules
 chroot $ROOTDIR apt-get install -y ca-certificates curl binutils git-core kmod
 wget https://raw.github.com/Hexxeh/rpi-update/master/rpi-update -O $ROOTDIR/usr/local/sbin/rpi-update
 chmod a+x $ROOTDIR/usr/local/sbin/rpi-update
-SKIP_WARNING=1 SKIP_BACKUP=1 ROOT_PATH=$ROOTDIR BOOT_PATH=$ROOTDIR/boot $ROOTDIR/usr/local/sbin/rpi-update
+SKIP_WARNING=1 SKIP_BACKUP=1 ROOT_PATH=$ROOTDIR BOOT_PATH=$ROOTDIR/boot/firmware $ROOTDIR/usr/local/sbin/rpi-update
+rm $ROOTDIR/boot/firmware/kernel*.img
+
+# Install Debian kernel
+#wget https://raw.githubusercontent.com/raspberrypi/tools/master/mkimage/imagetool-uncompressed.py -O imagetool-uncompressed.py
+#wget https://raw.githubusercontent.com/raspberrypi/tools/master/mkimage/args-uncompressed.txt -O args-uncompressed.txt
+#wget https://raw.githubusercontent.com/raspberrypi/tools/master/mkimage/boot-uncompressed.txt -O boot-uncompressed.txt
+#chmod a+x imagetool-uncompressed.py
+#./imagetool-uncompressed.py $ROOTDIR/boot/vmlinuz-*
+#cp kernel.img $ROOTDIR/boot/firmware/kernel.img
+#cp kernel.img $ROOTDIR/boot/firmware/kernel7.img
+#cp $ROOTDIR/boot/System.map-* $ROOTDIR/boot/firmware/
+#cp $ROOTDIR/boot/config-* $ROOTDIR/boot/firmware/
+#rm imagetool-uncompressed.py args-uncompressed.txt boot-uncompressed.txt
+
+# Install Raspbian's kernel
+$LINUXSOURCEDIR/scripts/mkknlimg $LINUXSOURCEDIR/arch/$KERNEL_ARCH/boot/Image $ROOTDIR/boot/firmware/kernel7.img
+cp $LINUXSOURCEDIR/arch/$KERNEL_ARCH/boot/dts/*.dtb* $ROOTDIR/boot/firmware/ || true
+cp $LINUXSOURCEDIR/arch/$KERNEL_ARCH/boot/dts/broadcom/*.dtb* $ROOTDIR/boot/firmware/ || true
+cp $LINUXSOURCEDIR/arch/$KERNEL_ARCH/boot/dts/overlays/*.dtb* $ROOTDIR/boot/firmware/overlays/
+cp $LINUXSOURCEDIR/arch/$KERNEL_ARCH/boot/dts/overlays/README $ROOTDIR/boot/firmware/overlays/
 
 # Install extra packages.
-chroot $ROOTDIR apt-get install -y apt-utils vim nano whiptail netbase less iputils-ping net-tools isc-dhcp-client man-db
-chroot $ROOTDIR apt-get install -y anacron fake-hwclock
+#chroot $ROOTDIR apt-get install -y apt-utils vim nano whiptail netbase less iputils-ping net-tools isc-dhcp-client man-db
+#chroot $ROOTDIR apt-get install -y anacron fake-hwclock
 
 # Install other recommended packages.
 #apt-get install ntp apt-cron fail2ban needrestart
