e51e9f09b0ff1b040885dc97cb40c26f3eff59d5
Valentin Lorentz <progval+git@progval.net>
2017-03-24 19:14:54 +0100

    Remove usage of rpi-update.

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,14 +9,15 @@ $SOURCEDIR/vmdebootstrap-raspi3/bin/vmdebootstrap \
     --distribution stretch \
     --mirror http://localhost:3142/ftp.fr.debian.org/debian \
     --image `date +raspbian64-%Y%m%d.img` \
-    --size 1000M \
+    --size 1200M \
     --bootsize 64M \
     --bootdirfmt=%s/boot/firmware \
     --boottype vfat \
     --root-password raspberry \
     --verbose \
-    --no-kernel \
+    --package raspi3-firmware \
     --no-extlinux \
     --hostname raspberry \
     --foreign /usr/bin/qemu-aarch64-static \
+    --debootstrapopts="components=main,non-free" \
     --customize "$SOURCEDIR/customize.sh"
